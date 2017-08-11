e6a4e549d662a575e3ee3c45c5c355299d070cdc
Benjamin Wasty <benny.wasty@gmail.com>
2017-08-11 22:04:17 +0200

    change model-index.json to be more explicit

--- generate-index.py
+++ generate-index.py
@@ -12,13 +12,11 @@ def generate_index(root):
         os.chdir(model)
         model_contents = os.listdir(".")
         gltf_variant_dirs = [d for d in model_contents if d.startswith("glTF")]
-        variants = []
+        variants = {}
         for variant_dir in gltf_variant_dirs:
-            if (not os.path.exists("{}/{}.gltf".format(variant_dir, model))
-                    and not os.path.exists("{}/{}.glb".format(variant_dir, model))):
-                print "WARNING: did not find model file for {} {}".format(variant_dir, model)
-                continue
-            variants.append(variant_dir)
+            model_file = [f for f in os.listdir(variant_dir)
+                          if f.endswith(".glb") or f.endswith(".gltf")][0]
+            variants[variant_dir] = model_file
         if "screenshot" not in model_contents:
             print "WARNING: no screenshot found for {}".format("model")
         index.append({
